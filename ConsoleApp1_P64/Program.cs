dff4210ac592f75dfca6b4ed967bca369e5afcc0
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-06-29 11:56:56 +0800

    [modi] P66練習題

--- ConsoleApp1_P64/Program.cs
+++ ConsoleApp1_P64/Program.cs
@@ -18,13 +18,13 @@ namespace ConsoleApp1_P64
     /// <summary>
     /// P66 列舉宣告
     /// </summary>
-    public enum AppleStore
+    public enum QQState
     {
         Online = 3,
         OffLine,
         Leave,
-        Busy,
-        TakeMe,
+        Busy,   
+        CallMe,
     }
 
     
@@ -83,37 +83,46 @@ namespace ConsoleApp1_P64
         /// </summary>
         static void P66()
         {
-            //列舉可以直接跟int類型轉換
+            //列舉轉int
             //將列舉的值拿出來給p66_Open
-            AppleStore p66_Open = AppleStore.OffLine;
+            QQState p66_Open = QQState.OffLine;
             //強制將列舉轉為int給p66_OpenInt
-            // int p66_OpenInt = (int)p66_Open;
-            int p66_OpenInt = int.Parse(p66_Open);
-            Console.WriteLine($"列舉轉int{p66_OpenInt}");
+            int p66_OpenInt = (int)p66_Open;
+            Console.WriteLine($"列舉轉int  {p66_OpenInt}");
             Console.ReadKey();
 
-            //int也可以直接轉成列舉資料；若不存在將直接輸出原始值
+            //列舉轉string
+            string p66_OpenString = p66_Open.ToString();
+            Console.WriteLine($"列舉轉字串  {p66_OpenString}");
+            Console.ReadKey();
+
+            //int轉列舉<若不存在將直接輸出原始值>
             //宣告p66_Int等於5
             int p66_Int = 5;
             //強制將5轉為列舉中第5個值給p66_Open_1
-            AppleStore p66_Open_1 = (AppleStore)p66_Int;
-            Console.WriteLine($"int轉列舉{p66_Open_1}");
+            QQState p66_Open_1 = (QQState)p66_Int;
+            Console.WriteLine($"int轉列舉  {p66_Open_1}");
             Console.ReadKey();
 
-            //列舉轉成字串
-            string p66_OpenString = p66_Open.ToString();
-            Console.WriteLine($"列舉轉字串{p66_OpenString}");
-            Console.ReadKey();
-
-            //將字串轉為列舉值；若不存在將當作異常
+            //string轉列舉<若不存在將當作異常>
             string p66_string = "5";
             //調用Parse這個方法，將字串轉為對應的列舉類型
             //Enum.Parse的第一個參數，寫typeof判斷要轉哪一個enum的類型
             //Enum.Parse的第二個參數，要轉換的字串
             //一定要進行宣告，將列舉值拿出來給p66_Open_2
-            AppleStore p66_Open_2 = (AppleStore)Enum.Parse(typeof(AppleStore), p66_string);
-            Console.WriteLine($"子串轉為列舉值{p66_Open_2}");
+            //(轉換的列舉類型)Enum.Parse(typeof(要轉換的列舉類型),要轉換的字串)
+            QQState p66_Open_2 = (QQState)Enum.Parse(typeof(QQState), p66_string);
+            Console.WriteLine($"字串轉為列舉值  {p66_Open_2}");
             Console.ReadKey();
+
+            //練習 提供用戶選擇狀態，接收後，將輸入的類型轉換成列舉類型，並印出
+            Console.WriteLine("請輸入狀態 Online、OffLine、Leave、Busy、CallMe,");
+            string p66_test = Console.ReadLine();
+
+            QQState p66_test_enum = (QQState)Enum.Parse(typeof(QQState), p66_test);
+            int p66_test_int = (int)p66_test_enum;
+            Console.WriteLine($"用戶輸入值，轉換成列舉值  {p66_test_enum} 再轉int {p66_test_int}");
+            Console.ReadLine();
         }
 
         /// <summary>
