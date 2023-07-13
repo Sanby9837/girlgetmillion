5fdf9c61938c08bc2b429cc7beff61306846a29e
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-07-13 18:05:59 +0800

    [Add]  Maps、title

--- ConsoleApp1_P90FlyOut/Program.cs
+++ ConsoleApp1_P90FlyOut/Program.cs
@@ -10,6 +10,34 @@ namespace ConsoleApp1_P90FlyOut
     {
         static void Main(string[] args)
         {
+            GameShow();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 使用靜態int陣列來宣告地圖
+        /// </summary>
+        public static int[] Maps = new int [100]; 
+
+        /// <summary>
+        /// 遊戲一開始的固定標題
+        /// </summary>
+        public static void GameShow()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("(^ρ^)/  Hello，Welcome Sanby's Fly Out");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("********************************************");
         }
     }
 }
