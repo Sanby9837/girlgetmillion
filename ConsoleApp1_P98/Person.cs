628dec38e6d3841da8e4f1fe520e930613a5cab8
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-07-18 17:10:47 +0800

    [Add] P98 物件導向

---
+++ ConsoleApp1_P98/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_P98
+{
+    /// <summary>
+    /// 類
+    /// </summary>
+    public class Person
+    {
+        public string _name; //這三個都是變量/字段
+        public int _age;
+        public char _gender;
+
+        
+        public void Self()
+        {
+            Console.WriteLine($"我叫{this._name}，今年{this._age}歲，是個{this._gender}生");
+            //加上this表示當前這個類的對象
+        }
+
+    }
+}
--- ConsoleApp1_P98/Program.cs
+++ ConsoleApp1_P98/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApp1_P98
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Where u go?");
+            int goesto = Convert.ToInt32(Console.ReadLine());
+            switch (goesto)
+            {
+                case 98:
+                    P98();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        static void P98()
+        {
+            //建立了一個類的物件
+            Person sanby = new Person();
+            sanby._name = "33";
+            sanby._age = 27;
+            sanby._gender = '女';
+
+            sanby.Self();
+            Console.ReadKey();
         }
     }
 }
