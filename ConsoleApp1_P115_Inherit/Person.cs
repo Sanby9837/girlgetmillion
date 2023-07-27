df23d20dd818861c5736ce69d901d5e9b00f5cd1
DESKTOP-K1IUH04\clud1 <Sanbylai@gmail.com>
2023-07-27 22:09:55 +0800

    [Add] P115繼承

---
+++ ConsoleApp1_P115_Inherit/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_P115_Inherit
+{
+    class Person
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        private char _gender;
+        public char Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public void Self()
+        {
+            Console.WriteLine($"大家好，我叫{Name},今年{Age}，是一位{Gender}生");
+        }
+    }
+}
