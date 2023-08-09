0a359d13aae01a81944e05d8093167112f12de8d
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-08-09 17:52:24 +0800

    [Modi] P126 HashTable練習

--- ConsoleApp1_P126/Program.cs
+++ ConsoleApp1_P126/Program.cs
@@ -13,6 +13,23 @@ namespace ConsoleApp1_P126
         public const String Simplified = "简体侦1语2";
         static void Main(string[] args)
         {
+            Console.WriteLine("Where u go?");
+            int go = Convert.ToInt32(Console.ReadLine());
+            switch (go)
+            {
+                case 126:
+                    P126();
+                    break;
+
+                default: break;
+            }
+        }
+
+        /// <summary>
+        /// Hashtable練習
+        /// </summary>
+        static void P126()
+        {
             Hashtable ht = new Hashtable();
             for (int i = 0; i < Traditional.Length; i++)
             {
