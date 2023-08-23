61680cbd5a14e7fd94bcc80eb2e8cf0b1b4166d0
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-08-23 13:12:26 +0800

    [Add] P149 Serializable

--- ConsoleApp1_P147/Program.cs
+++ ConsoleApp1_P147/Program.cs
@@ -10,6 +10,70 @@ namespace ConsoleApp1_P147
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("請問~你想要哪一個品牌的筆電呀");
+            string brand = Console.ReadLine();
+            NoteBook nb = GetNoteBook(brand);
+            nb.SayHello();
+            Console.ReadKey();
+        }
+
+        public static NoteBook GetNoteBook(string brand)
+        {
+            NoteBook nb = null;
+            switch (brand)
+            {
+                case "Lenovo":
+                    nb = new Lenovo();
+                    break;
+                case "Asus":
+                    nb = new Asus();
+                    break;
+                case "Acer":
+                    nb = new Acer();
+                    break;
+                case "Dell":
+                    nb = new Dell();
+                    break;
+            }
+            return nb;
         }
     }
+
+    public abstract class NoteBook
+    {
+        public abstract void SayHello();
+    }
+
+    public class Asus : NoteBook
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Asus筆電現身ㄌ");
+        }
+    }
+
+    public class Acer : NoteBook
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Acer筆電現身ㄌ");
+        }
+    }
+
+    public class Lenovo : NoteBook
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Lenovo筆電現身ㄌ");
+        }
+    }
+
+    public class Dell : NoteBook
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Dell筆電現身ㄌ");
+        }
+    }
+
 }
---
+++ ConsoleApp1_P149/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_P149
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("要去哪邊?");
+            int go = Convert.ToInt32(Console.ReadLine());
+            Person p = new Person();
+            p.Name = "Sanby";
+            p.Age = 28;
+            switch (go)
+            {
+                case 1: Fun1(p); break;
+                case 2: Fun2(); break;
+            }
+        }
+
+        static void Fun1(Person p)
+        {
+            using (FileStream fsWrite = new FileStream(@"C:\Users\User\Desktop\149.txt", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fsWrite, p);
+            }
+            Console.WriteLine("序列化成功");
+            Console.ReadKey();
+        }
+        static void Fun2()
+        {
+            using (FileStream frRead = new FileStream(@"C:\Users\User\Desktop\149.txt", FileMode.Open, FileAccess.Read))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Person pp = (Person)bf.Deserialize(frRead);
+                Console.WriteLine(pp.Name);
+                Console.WriteLine(pp.Age);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    [Serializable]
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+    }
+}
