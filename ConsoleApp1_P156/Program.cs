5c607ac8d9f2c1f4ddc7f0c2bd7b903d2a9846c8
DESKTOP-2HRSE38\User <sanbylai@gmail.com>
2023-08-24 16:29:55 +0800

    [Add] P159 Store

---
+++ ConsoleApp1_P156/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_P156
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable fly = new Bird();
+            fly.Fly();
+            Bird bird = new Bird();
+            bird.Fly();
+            Console.ReadKey();
+        }
+    }
+
+    public class Bird:IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("鳥會飛飛飛飛");
+        }
+        /// <summary>
+        /// 顯示實現介面
+        /// </summary>
+        void IFlyable.Fly()
+        {
+            Console.WriteLine("介面的飛飛");
+        }
+    }
+
+    public interface IFlyable
+    {
+        void Fly();
+    }
+}
---
+++ ConsoleApp1_P158 Store/Salt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_P158_Store
+{
+    internal class Salt : Product
+    {
+        public Salt(double price, double count, string id) : base(price, count, id)
+        {
+        }
+    }
+}
